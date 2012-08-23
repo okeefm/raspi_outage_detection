b1d117cdd6cf185f885c57721095ccb2f2e18b96
Michael O'Keefe <okeefm@rpi.edu>
2012-08-23 15:56:01 -0400

    Updated rod.py to make it work a lot better

--- rod.py
+++ rod.py
@@ -3,22 +3,31 @@ from time import localtime, strftime
 
 cam = Camera()
 num_outage_frames = 0
-color_threshhold = (10, 10, 10)
-outage_threshold = 300
-
-while(1):
-	img = cam.getImage()
-	miv = img.meanColor()
-	color_outage = false
-	if miv < color_threshold:
-		color_outage = true
-	if color_outage:
-		num_outage_frames += 1
-	else:
-		num_outage_frames = 0
-	if (num_outage_frames > outage_threshold):
-		outage_notify()
-		num_outage_frames = 0
+color_threshold = (20, 20, 20)
+outage_threshold = 30
 
 def outage_notify():
 	print "An Outage has occurred at: " + strftime("%Y-%m-%d %H:%M:%S", localtime())
+
+while(1):
+	#get the current frame from the camera
+	img = cam.getImage()
+	#img.show()
+	#find the mean pixel color (RGB) for the camera
+	miv = img.meanColor()
+	#If the mean image value is below some threshold set as a constant, declare an outage for black levels
+	outage = False
+	if miv < color_threshold:
+		outage = True
+	
+	#figure out if there's a motion outage (not yet implemented)
+	
+	#if there's an outage for this frame, add one to the outage counter	
+	if outage:
+		num_outage_frames += 1
+	else:
+		num_outage_frames = 0
+	#if the outage counter is above some threshold, notify somehow
+	if (num_outage_frames > outage_threshold):
+		outage_notify()
+		num_outage_frames = 0
